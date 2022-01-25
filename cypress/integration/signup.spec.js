0338517fb1eee875f26d3bc6f4c74e486f05caa5
elvesbd <elvesbd@yahoo.com>
2022-01-25 10:28:52 -0300

    separating the test mass from the main test

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,7 +1,6 @@
 import signup from '../pages/SignupPage'
 
 describe('Register', () => {
-
  /*  before(() => {
     cy.log('Tudo aqui é executado uma única vez ANTES de TODOS os casos de testes')
   })
@@ -17,56 +16,26 @@ describe('Register', () => {
   afterEach(() => {
     cy.log('Tudo aqui é executado sempre DEPOIS de CADA caso de teste')
   }) */
+  beforeEach(function() {
+    cy.fixture('deliveryman').then((man) => {
+      this.deliveryman = man
+    })
+  })
 
-  it('user be a delivery man', () => {
-    const deliveryman = {
-      name: 'João Brito',
-      cpf: '99833441300',
-      email: 'joao@test.com',
-      whatsapp: '85997381011',
-      address: {
-        postalcode: '60831070',
-        street: 'Rua Miguel Guimarães',
-        number: '50',
-        details: 'fundos',
-        district: 'Lagoa Redonda',
-        city_uf: 'Fortaleza/CE'
-      },
-      delivery_method: 'Moto',
-      cnh: 'cnh-digital.jpg'
-    }
-    
+  it('user be a delivery man', function()  {
     signup.go()
-    signup.fillForm(deliveryman)
+    signup.fillForm(this.deliveryman.signup)
     signup.submit()
     
     const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
     signup.modalContentShouldBe(expectedMessage)
   });
 
-  it('cpf entered invalid', () => {
-    const deliveryman = {
-      name: 'João Brito',
-      cpf: '998334413AA',
-      email: 'joao@test.com',
-      whatsapp: '85997381011',
-      address: {
-        postalcode: '60831070',
-        street: 'Rua Miguel Guimarães',
-        number: '50',
-        details: 'fundos',
-        district: 'Lagoa Redonda',
-        city_uf: 'Fortaleza/CE'
-      },
-      delivery_method: 'Moto',
-      cnh: 'cnh-digital.jpg'
-    }
-    
+  it('cpf entered invalid', function() {
     signup.go()
-    signup.fillForm(deliveryman)
+    signup.fillForm(this.deliveryman.cpf_invalid)
     signup.submit()
 
-    
     signup.alertMessageShouldBe('Oops! CPF inválido')
   });
 });
